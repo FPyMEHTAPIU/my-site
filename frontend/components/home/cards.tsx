4ec9d789fe93e378575eb304a2d3d6cf510477a7
Nick <kolya59264@gmail.com>
2024-12-09 21:16:51 +0200

    Align navigation buttons. Needed to fix cards' margins.

--- frontend/components/home/cards.tsx
+++ frontend/components/home/cards.tsx
@@ -120,32 +120,32 @@ const Cards = () => {
                 scrollbar={{draggable: true}}
                 className="cards-control-block"
             >
+                {!isMobile && (
+                    <>
+                        <button className="arrow-round left">
+                            <img
+                                src={isMobile ? "./arrows/arrow_left_white16.svg" :
+                                    isTablet ? "./arrows/arrow_left_white24.svg" :
+                                        "./arrows/arrow_left_white32.svg"}
+                                alt="arrow left"
+                                className="arrow"
+                            />
+                        </button>
+                        <button className="arrow-round right">
+                            <img
+                                src={isMobile ? "./arrows/arrow_right_white16.svg" :
+                                    isTablet ? "./arrows/arrow_right_white24.svg" :
+                                        "./arrows/arrow_right_white32.svg"}
+                                alt="arrow left"
+                                className="arrow"
+                            />
+                        </button>
+                    </>
+                )}
                 {projects.map((project: ProjectCardData) => (
                     Card(project)
                 ))}
             </Swiper>
-            {!isMobile && (
-                <>
-                    <button className="arrow-round left">
-                        <img
-                            src={isMobile ? "./arrows/arrow_left_white16.svg" :
-                                isTablet ? "./arrows/arrow_left_white24.svg" :
-                                    "./arrows/arrow_left_white32.svg"}
-                            alt="arrow left"
-                            className="arrow"
-                        />
-                    </button>
-                    <button className="arrow-round right">
-                        <img
-                            src={isMobile ? "./arrows/arrow_right_white16.svg" :
-                                isTablet ? "./arrows/arrow_right_white24.svg" :
-                                    "./arrows/arrow_right_white32.svg"}
-                            alt="arrow left"
-                            className="arrow"
-                        />
-                    </button>
-                </>
-            )}
             <div className="pagination-block"></div>
         </div>
     )
