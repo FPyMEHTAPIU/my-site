4476f950311254218e88e342366b97af27d99471
Nick <kolya59264@gmail.com>
2024-12-25 18:43:42 +0200

    Set contact form working.

--- frontend/components/input.tsx
+++ frontend/components/input.tsx
@@ -6,22 +6,32 @@ const InputForm = (
 ) => {
     const InputContainer = () => {
         return (
-            <div className='input-container'>
-                <input placeholder="Name"/>
-                <input placeholder="Email"/>
-                <textarea
+            <form action="https://api.web3forms.com/submit" method="POST" className='input-container'>
+                <input type="hidden" name="access_key" value="ACCESS_KEY"/> {/*TODO: change ACCESS_KEY*/}
+
+                <input name="Name" type="text" placeholder="Name" required/>
+                <input name="Email" type="email" placeholder="Email" required/>
+                <textarea name="Message"
                     placeholder="Message"
-                    style={ { height: isMobile || !isTablet ? 357 : 157 } }
+                    style={{height: isMobile || !isTablet ? 357 : 157}}
+                    required
                 />
+
+                <input type="hidden" name="redirect" value="https://web3forms.com/success"/>
+                <input name="Confirmation" type="checkbox" required/>
                 <button
                     className="button-primary"
-                    style= {
-                        { width: isMobile || isTablet ? '100%' : 550,
-                            justifySelf: 'stretch'}
-                    }>
+                    style={
+                        {
+                            width: isMobile || isTablet ? '100%' : 550,
+                            justifySelf: 'stretch'
+                        }
+                    }
+                    type="submit"
+                >
                     <p className="body-default black">Submit</p>
                 </button>
-            </div>
+            </form>
         )
     }
 
