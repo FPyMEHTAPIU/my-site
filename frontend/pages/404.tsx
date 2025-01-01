e74e28a74de310267a2f84606377749d2a7fc69c
Nick <kolya59264@gmail.com>
2025-01-01 15:37:26 +0200

    Change 404 page.

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -1,5 +1,5 @@
 import {useRouter} from "next/router";
-import {useEffect} from "react";
+import React, {useEffect} from "react";
 
 const page404 = () => {
     const router = useRouter();
@@ -9,11 +9,13 @@ const page404 = () => {
     }, []);
 
     return (
-        <main>
+        <main style={{height: "100vh", justifyContent: "unset"}}>
+            <div className="background-ellipse-green page404"></div>
+            <div className="background-ellipse-blue page404"></div>
             <p className="not-found">
                 {'4{}4'}
             </p>
-            <div className="oops-block">
+            <div className="oops-block" style={{textAlign: "center"}}>
                 <h2>Oops! Page Not Found</h2>
                 <p className="body-default">
                     Something went wrong. Please check the URL or go back to the homepage.
