8ce49afa753a880c0d9274a2f1d1624533c4d99c
Nick <kolya59264@gmail.com>
2025-01-15 15:12:19 +0200

    Replace image link to a direct one and change description in head.

--- frontend/components/Metadata/head.tsx
+++ frontend/components/Metadata/head.tsx
@@ -9,28 +9,26 @@ const HeadData = () => {
             <meta name="keywords" content="Nick Saveliev, portfolio, Full-Stack,
             Full-Stack Developer, developer, programmer, software engineer, front-end,
             front-end developer"/>
-            <meta name="description" content="Here you can see my skills and projects."/>
+            <meta name="description" content="Here you can explore my skills and see how
+            I’ve applied them in my projects."/>
             <meta name="theme-color" content="#161616"/>
             <link rel="icon" href="/favicon.svg"/>
 
-            <meta itemProp="name" content="Nick Saveliev's portfolio website"/>
-            <meta itemProp="description" content="Here you can see my skills and projects."/>
-            <meta itemProp="image"
-                  content="https://drive.google.com/file/d/1mWBbygHJdihVxDQwsyu3Z6oa7v9Mmec5/view"/>
-
             <meta property="og:type" content="website"/>
             <meta property="og:title" content="Nick Saveliev's portfolio website"/>
-            <meta property="og:description" content="Here you can see my skills and projects."/>
+            <meta property="og:description" content="Here you can explore my skills and see how I’ve
+            applied them in my projects."/>
             <meta property="og:url" content="https://nicksaveliev.vercel.app/"/>
             <meta property="og:image"
-                  content="https://drive.google.com/file/d/1mWBbygHJdihVxDQwsyu3Z6oa7v9Mmec5/view"
+                  content="https://drive.google.com/uc?export=view&id=1mWBbygHJdihVxDQwsyu3Z6oa7v9Mmec5"
             />
 
             <meta name="twitter:card" content="summary_large_image"/>
             <meta name="twitter:title" content="Nick Saveliev's portfolio website"/>
-            <meta name="twitter:description" content="Here you can see my skills and projects."/>
+            <meta name="twitter:description" content="Here you can explore my skills and see how I’ve
+            applied them in my projects."/>
             <meta name="twitter:image"
-                  content="https://drive.google.com/file/d/1mWBbygHJdihVxDQwsyu3Z6oa7v9Mmec5/view"/>
+                  content="https://drive.google.com/uc?export=view&id=1mWBbygHJdihVxDQwsyu3Z6oa7v9Mmec5"/>
         </Head>
     )
 }
